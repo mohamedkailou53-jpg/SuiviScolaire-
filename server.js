725c69dbc0999027fdcbee72aa783b07ff8f0561
mohamedkailou53-jpg <mohamedkailou53@gmail.com>
2026-09-17 19:57:06 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -306,8 +306,10 @@ app.put('/api/data', auth, (req, res) => {
 // FICHIERS STATIQUES (frontend)
 // ───────────────────────────────────────────────────────────
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
